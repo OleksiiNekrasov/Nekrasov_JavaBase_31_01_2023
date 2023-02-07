00e4a87e3241eb4771540c6ff82d8a1955e1300b
[иOleksii Nekrasov <[ mortransagency@gmail.com>
2023-02-07 16:50:33 +0000

    added in HW1 test

--- src/main/java/com/hillel/nekrasov/home_works/works1/HelloWorld.java
+++ src/main/java/com/hillel/nekrasov/home_works/works1/HelloWorld.java
@@ -3,6 +3,7 @@ package com.hillel.nekrasov.home_works.works1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello World");
+        System.out.print("Test");
     }
 
 
