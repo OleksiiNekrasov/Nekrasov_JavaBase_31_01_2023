5e951efd9ef4f376bc3441cffd1ec1b3c1a43407
[иOleksii Nekrasov <[ mortransagency@gmail.com>
2023-02-07 17:05:06 +0000

    added file test

---
+++ src/main/java/com/hillel/nekrasov/home_works/works4/Main.java
@@ -0,0 +1,8 @@
+package com.hillel.nekrasov.home_works.works4;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Test");
+    }
+}
