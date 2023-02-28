036946d20eb35301693bd5f330843f2d3a80167d
[иOleksii Nekrasov <[ mortransagency@gmail.com>
2023-02-28 22:47:01 +0000

    added file task

---
+++ src/main/java/com/hillel/nekrasov/home_works/works12/Person.java
@@ -0,0 +1,26 @@
+package com.hillel.nekrasov.home_works.works12;
+
+
+public class Person {
+    //  public static void main (String[] args) {
+    //*  System.out.println("Зателефонувати громадянинові " + name + surName + " з міста " + city + " можна за номером " + telephone);
+
+    //*   personInfo("Will ", "Smith", "New York", "2936729462846");
+    //*   personInfo("Jackie ", "Chan", "Shanghai", "12312412412");
+    //*  personInfo("Sherlock ", "Holmes", "London", "37742123513");
+
+    //}
+
+    //    static String personInfo(String name,String surName, String city, String telephone){
+    //        personInfo("Will ", "Smith", "New York", "2936729462846");
+    //         personInfo("Jackie ", "Chan", "Shanghai", "12312412412");
+    //        personInfo("Sherlock ", "Holmes", "London", "37742123513");
+    //        return personInfo() ;
+    //      System.out.println("Зателефонувати громадянинові " + name + surName + " з міста " + city + " можна за номером " + telephone);
+
+}
+
+
+
+
+
